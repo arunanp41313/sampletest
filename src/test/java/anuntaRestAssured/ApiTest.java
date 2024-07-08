290b90a44a96a8cbf890a00d5548788fdfca3d91
qatest41313 <qatest41313@gmail.com>
2024-07-08 07:29:01 -0400

    5th commit

--- src/test/java/anuntaRestAssured/ApiTest.java
+++ src/test/java/anuntaRestAssured/ApiTest.java
@@ -10,6 +10,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -60,6 +63,11 @@ public class ApiTest {
 	@Test(enabled=true)
 	private void apiMethod45() throws IOException 
 	{
+		// Launch browser
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/login.php/");
+		
+		//Create folder
 		System.out.println("done---------------1");
 		Date currentDate = new Date();
         // Define the format pattern and locale
@@ -88,6 +96,15 @@ public class ApiTest {
             System.out.println("Directory already exists: " + directory.getAbsolutePath());
         }
      	System.out.println("done---------------2");
+     	
+     	
+     	//take ss
+     	//METHOD-- 1 ( It requires common.io dependency for using fileutils
+     			TakesScreenshot screenshot= (TakesScreenshot) driver;
+     			File sourcefile= screenshot.getScreenshotAs(OutputType.FILE);
+     			String directoryPathSS = jenkinsWorkspace + "//" + formattedDate + "//screenshot1.jpg";
+     			File destinationfile= new File(directoryPathSS);
+     			FileUtils.copyFile(sourcefile, destinationfile);
     };
 			
 }
